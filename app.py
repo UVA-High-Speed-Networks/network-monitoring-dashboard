ea38d66e5f7c34d19f03654b96efbc1b49ae05fc
curtisxk38 <ck2ha@virginia.edu>
2018-05-18 09:33:45 -0400

    remove app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from dashboard.dashboard import app, socketio, config
-
-if __name__ == "__main__":
-    # debug = True causes app reload on py file change
-    # (instead of restarting Flask)
-  app.debug = False
-  socketio.run(app, port=config['port'])
