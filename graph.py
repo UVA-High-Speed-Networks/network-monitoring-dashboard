c27e968d38fc851309fa5313ec91110a04223099
curtisxk38 <ck2ha@virginia.edu>
2018-05-25 10:54:36 -0400

    fix date labels running together

--- graph.py
+++ graph.py
@@ -6,17 +6,17 @@ import pandas as pd
 import numpy as np
 
 def main():
-	tr, cap, stat = parse_to_df("../af-flawed-stats", "1", "em1")
+	tr, cap, stat = parse_to_df("../af-optimized", "3", "em1")
 	#tr, cap, stat = parse_to_df("../pf-sample-stats")
 	broctl(tr,stat)
 	statslog(tr,stat)
-	#ifconf(tr,stat)
+	ifconf(tr,stat)
 	capture_loss(tr,cap)
 
 def capture_loss(tr, cap):
 	fig, ax1 = plt.subplots()
 
-	fig.suptitle("Utilization, capture_loss percent_lost vs time")
+	fig.suptitle("Utilization, capture_loss rate vs time")
 
 	color = 'tab:red'
 
@@ -35,17 +35,19 @@ def capture_loss(tr, cap):
 	y2 = cap.percent_lost / 100
 	
 	ax2.plot_date(x2, y2, fmt="-", color=color)
-	ax2.set_ylabel("capture_loss percent_loss", color=color)
+	ax2.set_ylabel("capture_loss rate", color=color)
 	ax2.tick_params(axis="y", labelcolor=color)
 	ax2.set_ylim(0, 1)
 
-	ax1.set_xlim([x2[0], x2[-1]])
+	ax1.set_xlim([x1[0], x1[-1]])
+
+	fig.autofmt_xdate()
 
 	plt.savefig("capture_loss.png")
 def ifconf(tr, stat):
 	fig, ax1 = plt.subplots()
 
-	fig.suptitle("Utilization, ifconfig drop_rate vs time")
+	fig.suptitle("Utilization, if_config drop rate vs time")
 
 	color = 'tab:red'
 	
@@ -62,15 +64,17 @@ def ifconf(tr, stat):
 	color = 'tab:blue'
 
 	x2 = x1
-	y2 = tr.ifconfig_drop_rate
+	y2 = tr.ic_drop_rt
 	
 	ax2.plot_date(x2, y2, fmt="-", color=color)
-	ax2.set_ylabel("ifconfig drop_rate", color=color)
+	ax2.set_ylabel("if_config drop rate", color=color)
 	ax2.tick_params(axis="y", labelcolor=color)
 	ax2.set_ylim(0, 1)
 
 	lim = dates.date2num(stat.ts)
-	ax1.set_xlim([lim[0], lim[-1]])
+	ax1.set_xlim([x1[0], x1[-1]])
+
+	fig.autofmt_xdate()
 
 	plt.savefig("ifconfig_drop.png")
 
@@ -106,7 +110,9 @@ def broctl(tr, stat):
 	ax2.set_ylim(0, 1)
 
 	lim = dates.date2num(stat.ts)
-	ax1.set_xlim([lim[0], lim[-1]])
+	ax1.set_xlim([x1[0], x1[-1]])
+
+	fig.autofmt_xdate()
 
 	plt.savefig("broctl_packetLoss.png")
 
@@ -146,7 +152,9 @@ def statslog(tr, stat):
 	ax2.tick_params(axis="y", labelcolor=color)
 	ax2.set_ylim(0, 1)
 
-	ax1.set_xlim([x2[0], x2[-1]])
+	ax1.set_xlim([x1[0], x1[-1]])
+
+	fig.autofmt_xdate()
 
 	plt.savefig("statslog_pkts_dropped.png")
 
