cd4d46ec2ceff5b34bb25b6bb0647c539ce35a48
curtisxk38 <ck2ha@virginia.edu>
2018-05-25 12:58:59 -0400

    combine 2 datasets

--- graph.py
+++ graph.py
@@ -6,22 +6,25 @@ import pandas as pd
 import numpy as np
 
 def main():
-	tr, cap, stat = parse_to_df("../af-optimized", "3", "em1")
+	tr2, cap2, stat2 = parse_to_df("../af-optimized", "3", "em1")
+	tr1, cap1, stat1 = parse_to_df("../af-flawed-stats", "1", "em1")
 	#tr, cap, stat = parse_to_df("../pf-sample-stats")
-	broctl(tr,stat)
-	statslog(tr,stat)
-	ifconf(tr,stat)
-	capture_loss(tr,cap)
+	broctl(tr1,tr2,stat1,stat2)
+	statslog(tr1,tr2,stat1,stat2)
+	ifconf(tr2,stat2)
+	capture_loss(tr1,tr2,cap1,cap2)
 
-def capture_loss(tr, cap):
+def capture_loss(tr1,tr2,cap1,cap2):
 	fig, ax1 = plt.subplots()
 
 	fig.suptitle("Utilization, capture_loss rate vs time")
 
 	color = 'tab:red'
 
-	x1 = dates.date2num(tr.ts)
-	y1 = tr.utilization
+	x1 = tr1.ts.append(tr2.ts)
+
+	x1 = dates.date2num(x1)
+	y1 = tr1.utilization.append(tr2.utilization)
 	
 	ax1.plot_date(x1, y1, fmt="-", color=color)
 	ax1.set_ylabel("em1 utilization, GB/s", color=color)
@@ -31,8 +34,11 @@ def capture_loss(tr, cap):
 	ax2 = ax1.twinx()
 	color = 'tab:blue'
 
-	x2 = dates.date2num(cap.ts)
-	y2 = cap.percent_lost / 100
+	x2  = cap1.ts.append(cap2.ts)
+	x2 = dates.date2num(x2)
+
+	y2 = cap1.percent_lost.append(cap2.percent_lost)
+	y2 = y2 / 100
 	
 	ax2.plot_date(x2, y2, fmt="-", color=color)
 	ax2.set_ylabel("capture_loss rate", color=color)
@@ -78,16 +84,18 @@ def ifconf(tr, stat):
 
 	plt.savefig("ifconfig_drop.png")
 
-def broctl(tr, stat):
+def broctl(tr1,tr2,stat1,stat2):
 	fig, ax1 = plt.subplots()
 
 	fig.suptitle("Utilization, Broctl packetLoss vs time")
 
 	color = 'tab:red'
 	
-	
-	x1 = dates.date2num(tr.ts)
-	y1 = tr.utilization
+	x1 = tr1.ts.append(tr2.ts)
+
+	x1 = dates.date2num(x1)
+	y1 = tr1.utilization.append(tr2.utilization)
+
 	
 	ax1.plot_date(x1, y1, fmt="-", color=color)
 	ax1.set_ylabel("em1 utilization, GB/s", color=color)
@@ -98,34 +106,40 @@ def broctl(tr, stat):
 	color = 'tab:blue'
 
 
-	tr = tr[tr.packetLoss >= 0]
-	netstats_loss = tr.packetLoss.apply(pd.to_numeric, errors="coerce")
+	tr1 = tr1[tr1.packetLoss >= 0]
+	netstats_loss1 = tr1.packetLoss.apply(pd.to_numeric, errors="coerce")
 
-	x2 = dates.date2num(tr.ts)
-	y2 = netstats_loss
+	tr2 = tr2[tr2.packetLoss >= 0]
+	netstats_loss2 = tr2.packetLoss.apply(pd.to_numeric, errors="coerce")
+
+
+	x2 = x1
+	y2 = netstats_loss1.append(netstats_loss2)
 
 	ax2.plot_date(x2, y2, fmt="-", color=color)
 	ax2.set_ylabel("broctl packetLoss", color=color)
 	ax2.tick_params(axis="y", labelcolor=color)
 	ax2.set_ylim(0, 1)
 
-	lim = dates.date2num(stat.ts)
-	ax1.set_xlim([x1[0], x1[-1]])
+
 
 	fig.autofmt_xdate()
 
 	plt.savefig("broctl_packetLoss.png")
 
 
-def statslog(tr, stat):
+def statslog(tr1,tr2,stat1,stat2):
 	fig, ax1 = plt.subplots()
 
 	fig.suptitle("Utilization, Stat.log drop rate vs time")
 
 	color = 'tab:red'
 	
-	x1 = dates.date2num(tr.ts)
-	y1 = tr.utilization
+	x1 = tr1.ts.append(tr2.ts)
+
+	x1 = dates.date2num(x1)
+	y1 = tr1.utilization.append(tr2.utilization)
+
 	
 	ax1.plot_date(x1, y1, fmt="-", color=color)
 	ax1.set_ylabel("em1 utilization, GB/s", color=color)
@@ -135,8 +149,11 @@ def statslog(tr, stat):
 	ax2 = ax1.twinx()
 	color = 'tab:blue'
 
-	x2 = dates.date2num(stat.ts)
-	y2 = stat.pkts_dropped / (stat.pkts_dropped + stat.pkts_proc)
+	x2 = stat1.ts.append(stat2.ts)
+	x2 = dates.date2num(x2)
+	dropped = stat1.pkts_dropped.append(stat2.pkts_dropped)
+	proc = stat1.pkts_proc.append(stat2.pkts_proc)
+	y2 = dropped / (dropped + proc)
 
 	#y2 = test_series(len(stat))
 
